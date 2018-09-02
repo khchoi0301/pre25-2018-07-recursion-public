7adb87507d0411a62c0832c13210cdf8425ad8f1
kihong choi <khchoi0301@gmail.com>
2018-09-02 18:34:08 +0900

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //console.log(obj,typeof(obj),Array.isArray(obj));
+  
+  var newObj;  
+  var resArr=[];
+  var index;
+
+  if(index<0){
+    return
+  }
+
+  if((typeof(obj)=='string')){
+    newObj='"'+obj+'"';
+
+  } else if(obj==null||typeof(obj)=='boolean'||(typeof(obj)=='number')){
+    newObj = obj+""
+
+  } else if (Array.isArray(obj)){
+    index = obj.length
+    for(var i=0;i<obj.length;i++){
+      resArr.push(stringifyJSON(obj[i]))     
+    }
+    newObj='['+resArr.join(",")+']'
+    
+  } else if((typeof(obj)=='object')){
+    index = obj.length
+      for(var key in obj){
+        if(key!="undefined"&&key!=='functions'){
+        resArr.push(stringifyJSON(key));
+        resArr.push(':')
+        resArr.push(stringifyJSON(obj[key]));
+        resArr.push(',')
+        }
+      }
+      //console.log(resArr)
+      resArr.pop();
+      newObj = '{'+resArr.join("")+'}'
+      //console.log(newArr)
+    
+  } else {
+  newObj = obj+""
+  }  
+  index--;
+  //console.log(newObj,typeof(newObj),index);
+  return newObj
 };
+
+// function 일 때 어떻게 처리하는가??
